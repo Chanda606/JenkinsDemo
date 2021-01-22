61f817cf232152e1cf3f0f190992658411179a00
chandaverma <chandaverma@chandas-air.attlocal.net>
2021-01-21 22:02:11 -0600

    Added New Class

---
+++ SeleniumNewProject/src/testingClassProgram/ImageLinkExample.java
@@ -0,0 +1,25 @@
+package testingClassProgram;
+
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class ImageLinkExample {
+	
+	public static void main(String[] args) 
+	   {
+	       System.setProperty("webdriver.gecko.driver","/Users/chandaverma/Downloads/Drivers/geckodriver"); 
+	       
+	       String baseUrl = "https://www.aa.com/homePage.do";
+
+	       WebDriver driver = new FirefoxDriver();
+
+	       driver.get(baseUrl);
+
+	       driver.findElement(By.xpath(".//*[@id='heroSlide0']/img")).click();
+
+	    }
+
+}
