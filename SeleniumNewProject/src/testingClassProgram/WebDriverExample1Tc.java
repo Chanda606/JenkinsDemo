6d9c833d1295e1829e60952074c12c61702ebeb1
chandaverma <chandaverma@chandas-air.attlocal.net>
2021-01-21 21:38:18 -0600

    my first commit

---
+++ SeleniumNewProject/src/testingClassProgram/WebDriverExample1Tc.java
@@ -0,0 +1,27 @@
+package testingClassProgram;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class WebDriverExample1Tc 
+{
+	
+	public static void main(String[] args) {
+
+	// Configuring the system properties of chrome driver
+    System.setProperty("webdriver.chrome.driver", "/Users/chandaverma/Downloads/Drivers/chromedriver");
+
+	//Initializing the browser driver
+	WebDriver driver = new ChromeDriver();
+
+	//Navigating through the facebook  website
+	driver.get("https://facebook.com");
+
+	System.out.println("Selenium Webdriver Test Script in Chrome");
+	
+
+	}
+	}
+	 
+			
